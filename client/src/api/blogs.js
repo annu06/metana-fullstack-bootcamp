d59394923d8b850b04225d509381fe5527660905
AnuraagChetia <anuraagchetia@gmail.com>
2026-02-24 16:47:50 +0530

    fix: preserve /api proxy and improve blogs error handling

--- client/src/api/blogs.js
+++ client/src/api/blogs.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "/api/blogs";
+// if proxy isn't behaving, explicitly hit backend URL
+const BASE_URL = process.env.REACT_APP_API_URL
+  ? `${process.env.REACT_APP_API_URL}/blogs`
+  : "http://localhost:3000/api/blogs";
 
 export const getAllBlogs = async () => {
   const res = await axios.get(BASE_URL);
